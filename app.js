5af949f19b5be481905772fd363d5435d7b0d633
Araldi Ulises Garcia Feliz <aizendaisho@gmail.com>
2023-03-19 14:42:01 -0400

    almost done

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("hi")
+const abrir = document.querySelector(".abrir");
+const cerrar = document.querySelector(".cerrar");
+const elUl = document.querySelector(".el-ul");
+
+abrir.addEventListener("click", () => {
+  if (!elUl.classList.contains("mostrar")) {
+    elUl.classList.add("mostrar");
+    abrir.classList.add("hidden");
+    cerrar.classList.remove("hidden");
+  }
+});
+cerrar.addEventListener("click", () => {
+  if (elUl.classList.contains("mostrar")) {
+    elUl.classList.remove("mostrar");
+    cerrar.classList.add("hidden");
+    abrir.classList.remove("hidden");
+  }
+});
